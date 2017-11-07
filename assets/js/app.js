c07ee9c57eaeb365709f0ebb8a117fae3276a5b1
Sameer <sameer@mathad.com>
2017-11-07 20:56:00 +0530

    testnov7-2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,30 @@
 var options = [
-        set0 = ['Option 1','Option 2'],
-        set1 = ['First Option','Second Option','Third Option']
+        set0 = ['1','8978700050','Sameer','Mathad','@howzzaat','32','Javascript'],
+        set1 = ['2','Option','Second Option','Third Option']
     ];
 
-function makeUL(array) {
+function makeUL(array[i]) {
     // Create the list element:
-    var list = document.createElement('ul');
+    for(var j=0; j<array.length; j++){
+      var list = document.createElement('tr');
 
-    for(var i = 0; i < array.length; i++) {
+
+
+    for(var k = 0; k < array[j].length; k++) {
         // Create the list item:
-        var item = document.createElement('li');
+        var item = document.createElement('td');
 
         // Set its contents:
-        item.appendChild(document.createTextNode(array[i]));
+        item.appendChild(document.createTextNode(array[k]));
 
         // Add it to the list:
         list.appendChild(item);
     }
+  }
 
     // Finally, return the constructed list:
     return list;
 }
 
 // Add the contents of options[0] to #foo:
-document.getElementById('customerlist').appendChild(makeUL(options[0]));
+document.getElementById('cuslist').appendChild(makeUL(options[0]));
