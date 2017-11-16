8ff1fcf11f285ece19f08e9e10fd42a3b7f1f418
Sameer <sameer@mathad.com>
2017-11-16 17:22:39 +0530

    test

--- assets/js/error.js
+++ assets/js/error.js
@@ -1,9 +1,28 @@
-function changeElementClass(event) {
-  console.log(document.getElementById('focusedInput').value);
 
+document.querySelector('#username').onchange = validateInput;
+document.querySelector('#email').onchange = validateInput;
+
+function validateInput(){
+  var email = document.getElementById('email')
+  var spa = email.parentElement.children[1]
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))
+      {
+
+        //document.getElementById('testlabel').innerText = 'valid email';
+        //email.parentElement.children[1].className = "hint--right"
+        email.parentElement.className = "form-group has-success has-feedback"
+
+
+      }
+
+else    {
+  
+        email.parentElement.children[1].className = "hint--right"
+        console.log(email.parentElement.children[1]['aria-label'])
+        email.parentElement.className = "form-group has-error has-feedback"
+        console.log(email.parentElement.children[1]);
+  }
+  return false;
 }
 
-
-console.log(document.getElementById('focusedInput').value);
-
-document.getElementById('focusedInput').classList.add('has-success');
+//.right-addon .glyphicon { right: 0px;}
